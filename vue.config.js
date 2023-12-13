805790e0173510cee4e71c112d83a37b1b369c4f
JirayutB <jirayut.bal4ncez@mail.kmutt.ac.th>
2023-12-13 22:59:27 +0700

    Add production build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
   devServer: {
-    proxy: "http://localhost:5000",
-  }
+    proxy: "http://10.4.56.44:5000",
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/kanomfrontend/'
+    : '/'
 };
